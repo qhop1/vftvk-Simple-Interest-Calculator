b1dd9c39a4b30c99e5620354a940e77af9ea21ce
qhop1 <98664413+qhop1@users.noreply.github.com>
2022-10-26 01:09:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+// calculate the interest given the user inputs
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //get values from user input
+    var principal = document.getElementById("principal").value;
+    //check for positive number
+    if  ((principal <= 0) || isNaN(principal) ) 
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+
+      var rate = document.getElementById("rate").value;
+      var years = document.getElementById("years").value;
+
+      var interest = principal * years * rate /100;
+
+      var year = new Date().getFullYear()+parseInt(years);
+
+      //build result message
+      document.getElementById("result").innerHTML="If you deposit " + " <mark> " + principal + " </mark> " + " <br/>"
+      + "at an interest rate of " + " <mark> " + rate + "%" + " </mark> " + " <br/>"
+      + "You will receive an amount of " + " <mark> " + interest + " </mark> " + "," + " <br/>"
+      + "in the year " + " <mark> " + year + " </mark> " + " <br/>";
+    }
     
 }
-        
+
+// output value of slider 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
